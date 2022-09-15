bcd897001905d376dffdda6af5dec0a9b949ff7e
Inyiama-ifeanyi <inyiamaifeanyi@gmail.com>
2022-09-15 14:11:39 -0700

    Fixed betty style

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,12 @@
 #include "main.h"
 
 /**
- * mul: multiplies two integers
- * @a: int to be multiplied to b
- * @b: int to be multiplied to a
+ * mul - multiplies two integers
+ * @a: integer to be multiplied with b
+ * @b: integer to be multiplied with a
  * Return: the result of the operation
  */
 int mul(int a, int b)
 {
-	int c;
-
-	c = a * b;
-
-	return (c);
+	return (a * b);
 }
