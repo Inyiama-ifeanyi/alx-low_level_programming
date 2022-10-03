4f499ee6e01e0c715010546bb04ba03d7640c70b
Inyiama-ifeanyi <inyiamaifeanyi@gmail.com>
2022-10-03 10:18:00 -0700

    Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -> checks if the character is uppercase
+ * @c: a character argument
+ * Return: returns 1 and 0 depending on the condition
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
